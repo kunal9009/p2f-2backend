5e964fa319b646c8388c5e10976c3df5e7facbc6
kunal9009 <kunal@mahattaart.com>
2026-04-17 01:40:41 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGODB_URI);
+  const conn = await mongoose.connect(process.env.MONGO_URI);
   console.log(`MongoDB connected: ${conn.connection.host}`);
 };
 

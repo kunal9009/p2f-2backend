6ab96ed533a770e529ca3ae26b71355ace455198
kunal9009 <kunal@mahattaart.com>
2026-04-17 16:08:34 +0530

    Include password in user query for login

--- src/controllers/admin/authController.js
+++ src/controllers/admin/authController.js
@@ -6,7 +6,7 @@ exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email }).select('+password');
     if (!user) {
       return res.status(401).json({
         success: false,

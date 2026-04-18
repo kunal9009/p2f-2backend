a7fa9a038119eb9269fc7634b2abc2d329e563ea
kunal9009 <kunal@mahattaart.com>
2026-04-18 14:20:39 +0530

    Update authController.js

--- src/controllers/admin/authController.js
+++ src/controllers/admin/authController.js
@@ -24,7 +24,7 @@ exports.login = async (req, res) => {
 
     const token = jwt.sign(
       { id: user._id, role: user.role },
-      process.env.JWT_SECRET || 'secret123',
+      process.env.JWT_SECRET,
       { expiresIn: '1d' }
     );
 
